2240227d40d4bb9e37fce15889b8493228eb1b8a
申法宽 <sfk@live.cn>
2023-01-06 14:36:24 +0800

    优化播放

--- ViewModel/PlayViewModel.cs
+++ ViewModel/PlayViewModel.cs
@@ -13,17 +13,11 @@ partial class PlayViewModel
 {
     private readonly IAudioManager _audioManager;
     private IAudioPlayer _audioPlayer;
-    private CancellationTokenSource _cts;
-    private Task _loopTask;
-    private bool _another = false;
-
     private bool _playStatus;
 
     public PlayViewModel()
     {
         GetPermission();
-
-        _cts = new CancellationTokenSource();
 
         NowLocalMusic = new LocalMusic()
         {
@@ -47,35 +41,26 @@ partial class PlayViewModel
         LoadLocalMusics();
     }
 
-    [ObservableProperty] [NotifyPropertyChangedFor(nameof(NowMusicNameText))]
+    [ObservableProperty]
     private LocalMusic _nowLocalMusic;
 
     [ObservableProperty] private string _musicPath;
     [ObservableProperty] private string _playBtnImg;
+    [ObservableProperty] private bool _playBtnEnabled;
     [ObservableProperty] private bool _isRefreshing;
     [ObservableProperty] ObservableCollection<LocalMusic> _localMusics;
-
-    public string NowMusicNameText =>
-        string.IsNullOrWhiteSpace(NowLocalMusic.Name) ? "" : $"正在播放: {NowLocalMusic.Name.Replace(".mp3", "")}";
 
     [RelayCommand]
     async void Play(LocalMusic music)
     {
         var switchMusic = music.Id != NowLocalMusic.Id;
 
+        Debug.WriteLine($"就要播放的：{music.Id}: {music.Name}");
+        Debug.WriteLine($"刚刚在播的：{NowLocalMusic.Id}: {NowLocalMusic.Name}");
         // 设置当前播放的歌曲
-        NowLocalMusic = music;
-        if (NowLocalMusic.Id == -1 || string.IsNullOrWhiteSpace(NowLocalMusic.Name))
+        if (music.Id > -1)
         {
-            if (LocalMusics.Count == 0)
-            {
-                if (Application.Current == null) return;
-                Debug.Assert(Application.Current.MainPage != null, "Application.Current.MainPage != null");
-                await Application.Current.MainPage.DisplayAlert("提示", "没有歌曲, 请先下载", "这就去");
-                return;
-            }
-
-            NowLocalMusic = LocalMusics[0];
+            NowLocalMusic = music;
         }
 
         // 判断文件是否存在
@@ -87,24 +72,22 @@ partial class PlayViewModel
             return;
         }
 
-        _cts.Cancel();
-        // 列表点个(切歌)
+        // 列表点个(自动切歌)
         if (switchMusic)
         {
-            _audioPlayer = null;
-            // _audioPlayer?.Stop();
+            _audioPlayer?.Stop();
             _audioPlayer = _audioManager.CreatePlayer(File.OpenRead($"{MusicPath}{NowLocalMusic.Name}.mp3"));
+            _audioPlayer.Seek(0);
+            _audioPlayer.PlaybackEnded += PlaybackEnded;
             _audioPlayer.Play();
             _playStatus = true;
             PlayBtnImg = "stop.png";
-            _another = true;
         }
         else
         {
-            _another = false;
             // 获取播放器
             _audioPlayer ??= _audioManager.CreatePlayer(File.OpenRead($"{MusicPath}{NowLocalMusic.Name}.mp3"));
-
+            _audioPlayer.PlaybackEnded += PlaybackEnded;
             // 切换播放状态
             if (_playStatus)
             {
@@ -120,50 +103,12 @@ partial class PlayViewModel
             }
         }
 
-        _cts = new CancellationTokenSource();
-        await CheckPlayStatus();
-    }
-
-    private async Task CheckPlayStatus()
-    {
-        Debug.WriteLine($"another {_another} {DateTime.Now}");
-        try
-        {
-            Debug.WriteLine($"正在播放 1 {NowLocalMusic.Name} {DateTime.Now}");
-
-            _loopTask = Task.Run(() =>
-            {
-                Debug.WriteLine($"正在播放 2 {NowLocalMusic.Name} {DateTime.Now}");
-                while (_audioPlayer.IsPlaying)
-                {
-                    if (_cts.IsCancellationRequested)
-                    {
-                        Debug.WriteLine("取消了.....");
-                        break;
-                    }
-
-                    Debug.WriteLine($"正在播放 {NowLocalMusic.Name} {DateTime.Now}");
-                    Thread.Sleep(1000);
-                }
-
-                Debug.WriteLine($"停止播放了.......... {_another}");
-                if (!_another) return;
-                Debug.WriteLine($"another {true}..........");
-                Play(NowLocalMusic.Id == LocalMusics.Count - 1
-                    ? LocalMusics[0]
-                    : LocalMusics[NowLocalMusic.Id + 1]);
-            }, _cts.Token);
-
-            await _loopTask;
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e);
-        }
+        // _cts = new CancellationTokenSource();
+        // await LoopTask();
     }
 
     [RelayCommand]
-    void Refresh()
+    private void Refresh()
     {
         IsRefreshing = true;
         LocalMusics.Clear();
@@ -188,6 +133,12 @@ partial class PlayViewModel
                 Name = files[i].Name.Replace(".mp3", ""),
             });
         }
+
+        if (LocalMusics.Count > 0)
+        {
+            NowLocalMusic = LocalMusics[0];
+            PlayBtnEnabled = true;
+        }
     }
 
     private async void GetPermission()
@@ -207,5 +158,36 @@ partial class PlayViewModel
                 }
             }
         }
+    }
+
+    private void PlaybackEnded(object sender, EventArgs e)
+    {
+        Debug.WriteLine($"{NowLocalMusic.Name} 播放完成");
+        var next = NowLocalMusic.Id == LocalMusics.Count - 1
+            ? LocalMusics[0]
+            : LocalMusics[NowLocalMusic.Id + 1];
+        Debug.WriteLine($"next: {next.Name}");
+        Play(next);
+    }
+
+    [RelayCommand]
+    private void PrePlay()
+    {
+        if (NowLocalMusic.Id - 1 >= 0)
+        {
+            Play(LocalMusics[NowLocalMusic.Id - 1]);
+            return;
+        }
+
+        Play(LocalMusics[0]);
+    }
+
+    [RelayCommand]
+    private void NextPlay()
+    {
+        var next = NowLocalMusic.Id == LocalMusics.Count - 1
+            ? LocalMusics[0]
+            : LocalMusics[NowLocalMusic.Id + 1];
+        Play(next);
     }
 }
